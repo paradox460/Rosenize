8b9e1d1f936b84d1d4993c0ece3343cfcca3f172
Jeff Sandberg <paradox460@gmail.com>
2015-10-14 13:12:02 -0700

    Delete tests because its a rosen extension duhh

--- Gruntfile.js
+++ Gruntfile.js
@@ -158,15 +158,6 @@ module.exports = function (grunt) {
           ext: '.js'
         }]
       },
-      test: {
-        files: [{
-          expand: true,
-          cwd: 'test/spec',
-          src: '{,*/}*.coffee',
-          dest: './spec',
-          ext: '.js'
-        }]
-      }
     },
 
      // Compiles Sass to CSS and generates necessary files if requested
@@ -415,11 +406,6 @@ module.exports = function (grunt) {
     ]);
   });
 
-  grunt.registerTask('test', [
-    'connect:test',
-    'mocha'
-  ]);
-
   grunt.registerTask('build', [
     'clean:dist',
     'chromeManifest:dist',
@@ -438,7 +424,6 @@ module.exports = function (grunt) {
 
   grunt.registerTask('default', [
     'jshint',
-    'test',
     'build'
   ]);
 };
